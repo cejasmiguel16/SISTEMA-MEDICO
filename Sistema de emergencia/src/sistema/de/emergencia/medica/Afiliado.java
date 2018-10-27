d0cd1031f9999fc7aca0e6b6074e3c18b89143a6
cejasmiguel16 <42989205+cejasmiguel16@users.noreply.github.com>
2018-10-26 23:13:03 -0300

    AGREGANDO LOS ARCHIVOS

---
+++ Sistema de emergencia/src/sistema/de/emergencia/medica/Afiliado.java
@@ -0,0 +1,22 @@
+package sistema.de.emergencia.medica;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class Afiliado extends Persona{
+    private Integer numeroAfiliado;
+
+    public Afiliado(Integer numeroAfiliado, String Nombre, String apellido, Integer DNI, String sexo, int FechaNacimiento) {
+        super(Nombre, apellido, DNI, sexo, FechaNacimiento);
+        this.numeroAfiliado = numeroAfiliado;
+    }
+
+    public Integer getNumeroAfiliado() {
+        return numeroAfiliado;
+    }
+
+    public void setNumeroAfiliado(Integer numeroAfiliado) {
+        this.numeroAfiliado = numeroAfiliado;
+    }   
+}
---
+++ Sistema de emergencia/src/sistema/de/emergencia/medica/GestionHospital.java
@@ -0,0 +1,9 @@
+package sistema.de.emergencia.medica;
+
+import java.util.ArrayList;
+
+public class GestionHospital {
+          private ArrayList<Persona> personas;
+          private ArrayList<Movil> moviles;
+          
+}
